fcc1a5cd1bc553b827d724b65942eef1d0143e84
yono <yono233@outlook.com>
2024-09-07 16:30:44 +0800

    优化配置文件，符合CMSIS Configuration Wizard，以便于使用UI进行配置

--- common/include/MBx_user_sample.h
+++ common/include/MBx_user_sample.h
@@ -6,61 +6,88 @@
 #ifndef _MBX_USER_H_
 #define _MBX_USER_H_
 
-/**
- * 从机功能是否开启的配置项 0则为裁剪掉从机功能 >0则启用
- */
+//-------- <<< Use Configuration Wizard in Context Menu >>> -----------------
+
+// <h> 功能配置
+
+// <e> 从机功能是否开启
+// <o> 从机功能是否开启
+//   <0=> 裁剪去掉从机功能
+//   <1=> 从机启用
+// <i> Default: 从机启用
+#ifndef MBX_SLAVE_ENABLE
 #define MBX_SLAVE_ENABLE 1
+#endif
 
-/**
- * 主机功能是否开启的配置项 0则为裁剪掉主机功能 >0则启用
- */
+// </e>
+
+// <e> 主机功能是否开启
+// <o> 主机功能是否开启
+//   <0=> 裁剪去掉主机功能
+//   <1=> 主机启用
+// <i> Default: 主机启用
+#ifndef MBX_MASTER_ENABLE
 #define MBX_MASTER_ENABLE 1
+#endif
 
-/**
- * MBX的错误追踪模组是否启用 0则为裁剪掉错误追踪模组功能 >0则启用
- */
-#define MBX_MODULE_ERR_TRACE_ENABLE 1
-
-/**
- * MBX的发送port函数是否为多字节发送 0则需要绑定单字节发送port >0则需绑定多字节发送port
- */
-#define MBX_SEND_MODE_BYTES 1
-
-/**
- * MBX的CRC使用库内工具生成还是外部挂载 0则使用库内工具生成 >0则使用外部挂载
- * 若使用外部挂载则需 自行实现 uint16_t MBx_utility_crc16(uint8_t* data, uint16_t len); 用于实现CRC计算
- *                      MBx_utility_crc16.c 中有库内CRC生成算法及其传参定义
- */
-#define MBX_BUILT_UTILTY_CRC_ENABLE 1
-
-/**
- * MBX对于寄存器数据处理的大小端模式 0表示数据是小端存储 >0表示是大端
- *      多寄存器组合拼凑依赖此设置
- *      暂不支持完全依据大小端自动处理, 仍采用手动定义每个寄存器地址的属性
- */
-#define MBX_ENDIAN_MODE_BIG 0
-
-/***********主机特有属性定义***********/
-/**
- * MBX主机请求队列的最大深度，用于连续读写请求，寄存并自动发送处理 由于采用8位存储应不大于于255
- */
+//   <o> 主机请求队列的最大深度 <32-255>
+//   <i> 是队列中的最大寄存条数，用于连续读写请求，寄存并自动发送处理。是8位存储故而不超过255
+//   <i> Default: 40
 #define MBX_MASTER_REQUEST_QUEUE_MAX 40
 
-/**
- * MBX主机错误响应队列的最大深度，用于寄存从机的错误响应，并在主应用中取出处理 由于采用8位存储应不大于于255
- */
+//   <o> 主机错误响应队列的最大深度 <1-255>
+//   <i> 是队列中的最大寄存条数，用于寄存从机的错误响应，并在主应用中取出处理。是8位存储故而不超过255
+//   <i> Default: 10
 #define MBX_MASTER_ERROR_QUEUE_MAX 10
 
-/**
- * MBX主机读写多个寄存器的最大个数，由于回复"字节数"段是一个字节，所以配置最大值为0x7F
- */
-#define MBX_MASTER_MULTI_REG_MAX 0x7F
+//   <o> 主机读写多个寄存器的最大个数 <1-127>
+//   <i> 由于从机回复"字节数"段是一个字节，所以配置最大值为0x7F
+//   <i> Default: 127
+#define MBX_MASTER_MULTI_REG_MAX 127
 
-/**
- * MBX主机响应超时的判定阈值，以us为单位。
- *      若主机在设定时间内未收到从机响应，则判定为超时
- *      NOTE:未来可做到对象结构体中进行配置
- */
+//   <o> 主机响应超时的判定阈值[us] <10000-1000000000>
+//   <i> 若主机在设定时间内未收到从机响应，则判定为超时
+//   <i> Default: 100000
 #define MBX_MASTER_RESPONSE_TIMEOUT_US 100000
+
+// </e>
+
+// <o> 错误追踪模组是否开启
+//   <0=> 裁剪掉错误追踪模组
+//   <1=> 错误追踪模组启用
+// <i> Default: 错误追踪模组启用
+#ifndef MBX_MODULE_ERR_TRACE_ENABLE
+#define MBX_MODULE_ERR_TRACE_ENABLE 1
+#endif
+
+// </h>
+
+// <h> 属性配置
+
+// <o> 发送port函数是否为多字节发送
+//   <0=> 单字节发送port
+//   <1=> 多字节发送port
+// <i> Default: 多字节发送port
+#define MBX_SEND_MODE_BYTES 1
+
+// <o> CRC使用库内工具还是外部挂载函数生成
+//   <0=> 使用库内工具
+//   <1=> 使用外部挂载
+// <i> 若使用外部挂载则需 自行实现 uint16_t MBx_utility_crc16(uint8_t* data, uint16_t len); 用于实现CRC计算
+// <i> MBx_utility_crc16.c 中有库内CRC生成算法及其传参定义
+// <i> Default: 多字节发送port
+#define MBX_BUILT_UTILTY_CRC_ENABLE 1
+
+// <o> CPU的大小端
+//   <0=> 小端
+//   <1=> 大端
+// <i> 多寄存器组合拼凑依赖此设置
+// <i> 暂不支持完全依据大小端自动处理, 仍采用手动定义每个寄存器地址的属性
+// <i> Default: 小端
+#define MBX_ENDIAN_MODE_BIG 0
+
+// </h>
+
+//------------- <<< end of configuration section >>> -----------------------
 
 #endif /* _MBX_USER_H_ */
